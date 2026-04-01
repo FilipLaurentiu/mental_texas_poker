23b100c4b2b1ebe85bf0d92b39ac4098a94decf3
Filip Laurentiu <filiplaurentiu@proton.me>
2026-04-01 19:15:49 +0300

    Add initial PoC MPC program

---
+++ mpc/mental_poker_card_deal.py
@@ -0,0 +1,30 @@
+from Compiler import program
+from Compiler.util import bit_and, bit_xor
+
+community_cards_availability = int(program.args[1])
+
+# all 52 bits set to 1
+# MAX = 4503599627370495
+# test(community_cards_availability <= MAX, 1)
+
+# Number of players
+n = int(program.args[2])
+print_ln('Number of players %s', n)
+
+players_randomness = 0
+
+
+@for_range(n)
+def remove_unavailable_cards(i):
+    player_i = program.args[2 + i]
+    player_i_availability = player_i.0
+    player_i_randomness = player_i.1
+    # test(player_i_availability <= MAX, 1)
+    community_cards_availability = bit_and(
+        community_cards_availability, player_i_availability)
+    players_randomness = bit_xor(players_randomness, player_i_randomness)
+
+
+cards_availability_arr = sint.Array([i for i, val in enumerate(
+    community_cards_availability.bit_decompose(52)) if val == 1])
+cards_availability_arr[players_randomness % len(cards_availability_arr)]
